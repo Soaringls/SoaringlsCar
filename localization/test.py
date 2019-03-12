42b206d8a64c6b82ab522df789a77005b285fefd
Soaringls <upc_ls@163.com>
2019-03-12 18:29:44 +0800

    test

--- localization/test.py
+++ localization/test.py
@@ -1,13 +1,14 @@
+#Location直方图滤波算法实现与概率分布可视化 参考 https://zhuanlan.zhihu.com/p/46250526
 import numpy as np
 import matplotlib.pyplot as plt
-
+#初始化概率，定义每个网格颜色，由此一维网格地图初始化完毕
 p=[0.2,0.2,0.2,0.2,0.2]
-world=['green','red','red','green','green']
-
-measurements=['red','red']
-motions     =[1,1]
-pHit        =0.6  #测量值准确率
-pMiss       =0.2  #测量值失败率
+world=['green','red','red','green','green']  #各个位置的特征
+#加入传感器与运动
+measurements=['red','green'] #测量值
+motions     =[-1,1] #机器人移动方向,-1左 1右
+pHit        =0.6    #测量值正确的概率
+pMiss       =0.2    #测量值失败的概率
 pExact      =0.8
 pOvershoot  =0.1
 pUndershoot =0.1
@@ -26,8 +27,11 @@ def move(p, U):
     q = []
     for i in range(len(p)):
         s = pExact * p[(i - U)%len(p)]
+        print("index1 is "+str((i - U)%len(p)))
         s = s + pOvershoot * p[(i-U-1)%len(p)]
+        # print("index2 is------"+str((i-U-1)%len(p)))
         s = s + pUndershoot* p[(i-U+1)%len(p)]
+        # print("index3 is------------ "+str((i-U+1)%len(p)))
         q.append(s)
     return q
 def display_map(grid, bar_width = 1):
@@ -43,20 +47,21 @@ def display_map(grid, bar_width = 1):
     else:
         print('Grid is empty')
 def main():
-    # global p
-    # for k in range(len(measurements)):
-    #     p = sense(p, measurements[k])
-    #     p = move(p, motions[k])
-    # print(p)
-    # display_map(p)
     global p
-    plt.ion()
     for k in range(len(measurements)):
         p = sense(p, measurements[k])
-        for i in range(1000):
-            p = move(p, motions[k])
-            plt.pause(0.2)
-            display_map(p, 0.9)
+        p = move(p, motions[k])
     print(p)
+    display_map(p)
+
+    # global p
+    # plt.ion()
+    # for k in range(len(measurements)):
+    #     p = sense(p, measurements[k])
+    #     for i in range(1000):
+    #         p = move(p, motions[k])
+    #         plt.pause(0.2)
+    #         display_map(p, 0.9)
+    # print(p)
 if __name__=='__main__':
     main()
